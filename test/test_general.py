a9cadf0cbd15b45c2c35b1ded8de878407fcb2b5
Nick Lalic <nick.lalic@gmail.com>
2016-12-27 19:53:18 -0800

    fixed typo

--- test/test_general.py
+++ test/test_general.py
@@ -5,10 +5,10 @@ from rohdeschwarz.instruments.vna import Vna
 from rohdeschwarz.instruments.vna import TraceFormat
 
 @ddt
-class TestVnaTrace(unittest.TestCase):
+class TestGeneral(unittest.TestCase):
     @data({'value': 0.5,    'units': Units.dB,      'result': '0.5 dB'},
           {'value': 5.0e-9, 'units': Units.seconds, 'result': '5.0 ns'},
-          {'value': 10.0,     'units': Units.none,  'result': '10.0 U'})
+          {'value': 10.0,   'units': Units.none,    'result': '10.0 U'})
     def test_trace_format_units(self, data):
         self.assertEqual(format_value(data['value'], data['units']), data['result'])
 
