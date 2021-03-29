a9982441047918ab4d94eba89edf6c177582a955
Nick Lalic <nick.lalic@gmail.com>
2021-03-29 00:48:46 -0600

    ♻️  added windows visa library path, clarified path for macos

--- rohdeschwarz/bus/visa/paths.py
+++ rohdeschwarz/bus/visa/paths.py
@@ -1,5 +1,16 @@
-macos_rsvisa_library_path = '/Library/Frameworks/RsVisa.framework/Versions/Current/RsVisa/librsvisa.dylib'
+from   ctypes.util import find_library
+import platform
+
+
+WIN_VISA_LIBRARY   = 'visa32'
+MACOS_VISA_LIBRARY = 'rsvisa'
 
 
 def get_visa_path():
-    return macos_rsvisa_library_path
+    system = platform.system()
+    if system == 'Windows':
+        # load from path
+        return WIN_VISA_LIBRARY
+    if system == 'Darwin':
+        # load from specific file
+        return find_library(MACOS_VISA_LIBRARY)
