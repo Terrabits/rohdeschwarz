58043178d40c48477cef093e1293ed60d95c71e8
Nick Lalic <nick.lalic@gmail.com>
2021-03-29 01:41:44 -0600

    ♻️  added linux support (untested)

--- rohdeschwarz/bus/visa/paths.py
+++ rohdeschwarz/bus/visa/paths.py
@@ -2,15 +2,12 @@ from   ctypes.util import find_library
 import platform
 
 
-WIN_VISA_LIBRARY   = 'visa32'
-MACOS_VISA_LIBRARY = 'rsvisa'
+WIN_VISA_LIBRARY = 'visa32'
+RS_VISA_LIBRARY  = 'rsvisa'
 
 
 def get_visa_path():
-    system = platform.system()
-    if system == 'Windows':
-        # load from path
+    if platform.system() == 'Windows':
         return WIN_VISA_LIBRARY
-    if system == 'Darwin':
-        # load from specific file
-        return find_library(MACOS_VISA_LIBRARY)
+    # else
+    return find_library(RS_VISA_LIBRARY)
